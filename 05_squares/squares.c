7d53aea4a9bbcb09bd9ac2d64c3bc5c8d9ec2b5f
Coursera Learner <nobody@nowhere.nul>
2020-01-26 19:23:08 +0000

    Translated assignment 05 algorithm into code, but not working correctly

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -8,8 +8,11 @@
  */
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
+  if(coord >= offset && coord<(offset+size)){
+    return 1;
+  }
   // else, return 0
-  return 0;
+   return 0;
 }
 
 /*
@@ -18,20 +21,36 @@ int isInRange(int coord, int offset, int size) {
  */
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
+  if(coord == offset || coord == (offset+size)){
+    return 1;
+  }
   // return 1, else return 0
   return 0;
 }
+//compute max of two numbers
+int max(int num1, int num2){
+  if(num1>num2){
+    return num1;
+  }
+  return num2;
+ }
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w = max(size1, (x_offset+size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h = max(size1, (y_offset+size2));
   //count from 0 to h. Call the number you count with y
-
+  for(int y=0; y < h; y++){
     //count from 0 to w. Call the number you count with x
-
+    for(int x=0; x < w; x++){
       //check if  EITHER
+      
+      if ( ((isInRange(x,x_offset, (x_offset+size2)) == 1) && (isAtBorder(y,y_offset,(y_offset+size2-1))==1)) || ((isInRange(y, y_offset, (y_offset+size2))==1) && (isAtBorder(x, x_offset, (x_offset+size2-1))==1 )) )
+	{
+	printf("*");
+      }
+
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
       //  OR
@@ -41,13 +60,24 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
 
       //if not,
       // check if EITHER
+      else if(((x < size1) && (y==0 || y==(size1-1))) || ((y<size1) && (x==0 || x == (size1-1)))){
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
+	printf("#");
+      }
 
       //else print a space
+      else{
+	printf(" ");
+      }
+
+    }
+
+    printf("\n");
     //when you finish counting x from 0 to w, 
     //print a newline
+  }
 
 }
