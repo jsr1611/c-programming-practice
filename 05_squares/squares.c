fb838d5e76017d7d4e019c44fb5e17a84356fc1e
Coursera Learner <nobody@nowhere.nul>
2020-03-14 14:17:39 +0000

    Fifth problem revised

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -8,7 +8,7 @@
  */
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
-  if(coord >= offset && coord < (offset+size)){
+  if(coord >= offset && coord < (size)){
     return 1;
   }
   // else, return 0
@@ -52,7 +52,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     for(int x=0; x < w; x++){
       //check if  EITHER
       
-      if ((isInRange(x, x_offset, (x_offset+size2)) == 1 && isAtBorder(y,y_offset,(y_offset+size2 - 1)))== 1 || (isInRange(y, y_offset, (y_offset+size2))==1 && isAtBorder(x, x_offset, (x_offset+size2 - 1))==1 ))
+      if ((isInRange(x, x_offset, (x_offset+size2)) == 1 && isAtBorder(y,y_offset,(y_offset+size2 - 1))== 1) || (isInRange(y, y_offset, (y_offset+size2))==1 && isAtBorder(x, x_offset, (x_offset+size2 - 1))==1 ))
 	{
 	printf("*");
       }
