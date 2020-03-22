497eb56a9c3c9eaff5ce388e5bd8913d2a7b99c5
Coursera Learner <nobody@nowhere.nul>
2020-03-22 14:39:58 +0000

    initial commit for 07_assignment

---
+++ 07_retirement/retirement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct _retire_info(int months, double contribution, double rate_of_return){
+  int months;
+  double contribution;
+  double rate_of_return;
+};
+
+typedef struct _retire_info retire_info;
+
+void retirement(int startAge, double initial, retire_info working, retire_info retired){
+
+
+}
