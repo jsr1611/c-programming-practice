9030f38c881a5e6df34573b9ce5d5342b2517ebc
Coursera Learner <nobody@nowhere.nul>
2020-03-25 11:49:22 +0000

    first commit

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,91 @@
 
 
 void assert_card_valid(card_t c) {
-
-
+  assert((c.value >= 2) || (c.value <= 14));
+  assert((c.suit == SPADES) || (c.suit == HEARTS) || (c.suit == DIAMONDS) || (c.suit == CLUBS));
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  // first 3 are flop, 4th = turn, 5th = river
+  switch(r){
+  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH"; break;
+  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND"; break;
+  case FULL_HOUSE: return "FULL_HOUSE"; break;
+  case FLUSH: return "FLUSH"; break;
+  case STRAIGHT: return "STRAIGHT"; break;
+  case THREE_OF_A_KIND: return "THREE_OF_A_KIND"; break;
+  case TWO_PAIR: return "TWO_PAIR"; break;
+  case PAIR: return "PAIR"; break;
+  default : return "Nothing"; break;
+  }
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  switch(c.value){
+ case 0:  return 50;  break;
+ case 1:  return 51;  break;
+ case 2:  return 52;  break;
+ case 3:  return 53;  break;
+ case 4:  return 54;  break;
+ case 5:  return 55;  break;
+ case 6:  return 56;  break;
+ case 7:  return 57;  break;
+ case 8: return 48;  break;
+ case 9: return 74;  break;
+ case 10: return 81;  break;
+ case 11: return 75;  break;
+ default: return 65;  break;   
+ }
 }
 
-
 char suit_letter(card_t c) {
-  return 'x';
-  
+  switch(c.suit){
+
+  case SPADES:    return 115;    break;
+  case HEARTS:    return 104;   break;
+  case DIAMONDS:  return 100;    break;
+  default:        return  99;   break;
+  }
 }
 
 void print_card(card_t c) {
-
+  printf("%d%d", value_letter(c), suit_letter(c));  
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  assert(value_let);
+  assert(suit_let);
+  temp.value = value_let;
+  temp.suit = suit_let;
   return temp;
 }
 
 card_t card_from_num(unsigned c) {
   card_t temp;
+  card_t tmp;
+
+  if (c<13){
+    tmp.value = c;
+    temp.value = value_letter(tmp);
+    temp.suit = SPADES;
+  }
+  else if (c>=13 || c<26){
+    tmp.value = c%13;
+    temp.value = value_letter(tmp);
+    temp.suit = HEARTS;
+      }
+  else if(c>=26 || c<39){
+    tmp.value = c%13;
+    temp.value = value_letter(tmp);
+    temp.suit = DIAMONDS;
+  }
+  else
+    {
+      tmp.value = c%13;
+      temp.value = value_letter(tmp);
+      temp.suit = CLUBS;
+    }
+  
   return temp;
 }
