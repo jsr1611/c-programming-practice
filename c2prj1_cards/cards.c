eee18dadf3d03e73ca0166fe7e6d900ac1abb1b1
Coursera Learner <nobody@nowhere.nul>
2020-03-26 06:13:05 +0000

    cards.c

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -52,13 +52,28 @@ void print_card(card_t c) {
 }
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
-  temp.value = value_let;
-  temp.suit = suit_let;
-  
-  //assert_card_valid(temp);
-
-  temp.value = value_letter(temp);
-  temp.suit = suit_letter(temp);
+  switch(value_let){
+  case '2': temp.value = 2; break;
+  case '3': temp.value = 3; break;
+  case '4': temp.value = 4; break;
+  case '5': temp.value = 5; break;
+  case '6': temp.value = 6; break;
+  case '7': temp.value = 7; break;
+  case '8': temp.value = 8; break;
+  case '9': temp.value = 9; break;
+  case '0': temp.value = 10; break;
+  case 'j': temp.value = VALUE_JACK; break;
+  case 'Q': temp.value = VALUE_QUEEN; break;
+  case 'K': temp.value = VALUE_KING; break;
+  case 'A': temp.value = VALUE_ACE; break;        
+  }
+  switch(suit_let){
+  case 's': temp.suit = SPADES; break;
+  case 'h': temp.suit = HEARTS; break;
+  case 'd': temp.suit = DIAMONDS; break;
+  case 'c': temp.suit = CLUBS; break;    
+  }
+    assert_card_valid(temp);
   return temp;
 }
 
